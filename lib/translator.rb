0b6bed7da5b81477d30ec3a36524fcc7486f2089
Allene Norton <norton.allene@gmail.com>
2020-06-30 20:53:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(lib)
   loadlib.each do |meaning, vray|
     english_emoticon = vray[0] 
     japanese_emoticon = vray[1]
-    get_meaning[meaning.to_sym] = {english: english_emoticon, japanese: japanese_emoticon}
+    get_meaning[meaning] = {english: english_emoticon, japanese: japanese_emoticon}
     #get_emoticon[vray[0].to_sym] = vray[1]
   end
   result = get_meaning
