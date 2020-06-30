69c8827cc45879f129b6a50280191a8eee459c38
Allene Norton <norton.allene@gmail.com>
2020-06-30 20:10:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,17 +3,7 @@ require "pry"
 
 
 def load_library(lib)
-  get_emoticon = {}
-  get_meaning = {}
-  loadlib = YAML.load_file(lib)
-  loadlib.each do |k, v|
-    get_meaning[v[1]] = k
-    get_emoticon[v[0]] = v[1]
-  end
-  result = {}
-  result["get_meaning"] = get_meaning
-  result["get_emoticon"] = get_emoticon
-  result
+
 end
 
 def get_japanese_emoticon
