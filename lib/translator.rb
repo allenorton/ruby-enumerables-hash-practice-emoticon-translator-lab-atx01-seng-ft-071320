6b1a1948653ff2f0e9b34e5737c7a984b05db03c
Allene Norton <norton.allene@gmail.com>
2020-06-30 20:23:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,13 @@ require "pry"
 
 def load_library(lib)
   loadlib =YAML.load_file(lib)
-  library = {
-    'get_meaning'  => {},
-    'get_emoticon' => {} 
-    
-  }
+  'get_meaning'  => {}
+  'get_emoticon' => {} 
+  
   loadlib.each do |meaning, vray|
     library["get_meaning"][meaning.to_sym] = vray
     library["get_emoticon"][vray[0]] = vray[1]
   end
-  library 
   #binding.pry
 end
 
