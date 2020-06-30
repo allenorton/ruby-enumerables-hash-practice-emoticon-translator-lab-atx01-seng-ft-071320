17cef376694cec99a6b34966a40672709d1cb2de
Allene Norton <norton.allene@gmail.com>
2020-06-30 21:19:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,15 +4,18 @@ require "pry"
 
 def load_library(yaml_file)
   loadlib =YAML.load_file(yaml_file)
-  get_meaning  = {}
-  get_emoticon = {}
+  library = {
+    get_meaning  = {},
+    get_emoticon = {}
+    
+  }
   
   loadlib.each do |meaning, vray|
     english_emoticon = vray[0] 
     japanese_emoticon = vray[1]
     get_meaning[meaning] = {english: english_emoticon, japanese: japanese_emoticon}
   end
-  get_meaning
+  library
   #binding.pry
 end
 
