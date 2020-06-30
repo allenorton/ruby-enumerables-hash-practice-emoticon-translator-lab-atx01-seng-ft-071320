e164a74343765c732f65a7d1c33d070348e25650
Allene Norton <norton.allene@gmail.com>
2020-06-30 22:10:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,6 @@ def get_english_meaning(yaml_file, j_emoticons)
     langs.each do |in_key, emoti|
       if emoti == j_emoticons
         english_meaning = meaning
-      end
       elsif emoti != j_emoticons
         english_meaning = sorry
       end 
