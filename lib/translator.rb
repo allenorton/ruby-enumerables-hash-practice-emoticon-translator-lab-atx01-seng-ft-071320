24e28127222d115c2a38727b6f48d8bd151fd31a
Allene Norton <norton.allene@gmail.com>
2020-06-30 19:20:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(lib)
   loadlib =YAML.load_file(lib)
   library = {'get_meaning'  => {},
                   'get_emoticon' => {} }
-  loadlib.each do |meaning, emoticon|
+  loadlib.each do |meaning, vray|
     english = vray[0]
     japanese = vray[1]
   end
